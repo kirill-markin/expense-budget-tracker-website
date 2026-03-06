0a3a1f6ffd02e1714203687386692983cc49b335
Kirill Markin <markinkirill@gmail.com>
2026-03-06 12:36:13 +0100

    Update layout structure to enhance accessibility and styling
    - Moved the className for the HTML element to improve styling consistency.
    - Maintained the existing structure of the body element for clarity and organization.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,8 +49,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={sourceSerif.variable}>
+    <html lang="en" className={sourceSerif.variable}>
+      <body>
         <JsonLdSchema />
         <Header />
         <main>{children}</main>
